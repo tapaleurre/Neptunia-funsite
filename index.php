eafc403d3fa1783d85c08a136116135a4281dff4
Simon <simonfoex@hotmail.f>
2018-10-28 20:03:35 +0100

    commit

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ body, html {height: 100%}
     </div>
     <div class="w3-container">
       <h5>Hyperdimension neptunia episode 1-3 soon:</h5>
-	  <iframe src="http://free.timeanddate.com/countdown/i6h8lhkd/n195/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2018-10-29T22:30:00" allowTransparency="true" frameborder="0" width="320" height="135"></iframe>
+<iframe src="http://free.timeanddate.com/countdown/i6h8lhkd/n195/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2018-10-28T22:30:00" allowTransparency="true" frameborder="0" width="700" height="200"></iframe>
 	  <a href="rabbit.gya.neptunia.chat">Join session!</a>
     </div>
   </div>

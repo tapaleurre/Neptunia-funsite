e4770d9707d94a339d5f5e3daf593717e0a7fbde
Simon <simonfoex@hotmail.f>
2018-10-30 00:05:09 +0100

    timer updated

--- index.php
+++ index.php
@@ -145,8 +145,12 @@ body, html {height: 100%}
       <h1>Join us to watch anime together</h1>
     </div>
     <div class="w3-container">
-      <h5>Hyperdimension neptunia episode 1-3 soon:</h5>
-<iframe src="http://free.timeanddate.com/countdown/i6h8lhkd/n195/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2018-10-28T22:30:00" allowTransparency="true" frameborder="0" width="700" height="200"></iframe>
+      <h5>Hyperdimension neptunia episode 1-4 soon:</h5>
+<iframe src="http://free.timeanddate.com/countdown/i6hasf7a/n195/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2018-10-30T02:00:00" allowTransparency="true" frameborder="0" width="181" height="69"></iframe>
+<br><h5>Other chance to see them:</h5>
+<iframe src="http://free.timeanddate.com/countdown/i6hasf7a/n195/cf12/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pcfff/tcfff/fs100/szw320/szh135/tatTime%20left%20to%20Event%20in/tac000/tptTime%20since%20Event%20started%20in/tpc000/mac000/mpc000/iso2018-10-30T20:30:00" allowTransparency="true" frameborder="0" width="181" height="69"></iframe>
+<br><h5>EP 5-8 Coming soon.</h5>
+
 	  <a href="rabbit.gya.neptunia.chat">Join session!</a>
     </div>
   </div>
